28a28f6a52a52f0336ad9898f0b8b1ac05d935b0
kadirhan54 <kadirhansaglam5454@gmail.com>
2025-01-08 16:54:39 +0300

    navbar added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -10,6 +12,13 @@ Rails.application.routes.draw do
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
+  # get "pages/about"
+  # get "pages/services"
+  # get "pages/contact"
+
   # Defines the root path route ("/")
   root "posts#index"
+  get "about", to: "pages#about"
+  get "services", to: "pages#services"
+  get "contact", to: "pages#contact"
 end
